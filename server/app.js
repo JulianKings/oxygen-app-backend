665bb693eb6bbc84afcb836a11b63b9807da054f
JulianKings <julli123@hotmail.es>
2024-06-19 19:47:08 +0200

    Deploy basic app

---
+++ server/app.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import 'dotenv/config'
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import indexRouter from './routes/index';
+
+var app = express();
+
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URI);
+}
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    console.log(err);
+  });
+  
+  export default app;
